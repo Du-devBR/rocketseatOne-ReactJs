22dff01204d9bde8304be42110c9bf55a3522dc2
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-02 20:24:21 -0300

    feat: criado estrutura home

--- projeto04-ignite-shop/src/pages/index.tsx
+++ projeto04-ignite-shop/src/pages/index.tsx
@@ -1,29 +1,26 @@
-import {styled} from '../styles/index'
-
-
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
-
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import camiseta1 from '../assets/Shirt/1.png'
 
 export default function Home() {
   return (
     <div>
-      <Button>
-        <span>Teste</span>
-        Enviar
-      </Button>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} alt="" width={520} height={480}/>
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$79,70</span>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta1} alt="" width={520} height={480}/>
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$79,70</span>
+          </footer>
+        </Product>
+      </HomeContainer>
     </div>
   )
 }
