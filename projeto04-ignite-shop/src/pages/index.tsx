ab51998babed51d70d5b3621831997ea530d565e
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-02 10:01:57 -0300

    feat: criado pagina styles e configurado stitches

--- projeto04-ignite-shop/src/pages/index.tsx
+++ projeto04-ignite-shop/src/pages/index.tsx
@@ -1,8 +1,29 @@
+import {styled} from '../styles/index'
+
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello word </h1>
+      <Button>
+        <span>Teste</span>
+        Enviar
+      </Button>
     </div>
   )
 }
