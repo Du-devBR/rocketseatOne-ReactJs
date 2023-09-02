c86eae1d52d2b643c6c51dc97f64e24f5ee8aaea
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-02 13:57:18 -0300

    feat: definido style guide

--- projeto04-ignite-shop/src/styles/index.ts
+++ projeto04-ignite-shop/src/styles/index.ts
@@ -4,6 +4,15 @@ export const { config, styled, globalCss, keyframes, getCssText, theme, createTh
   theme: {
     colors: {
       rocketseat: '#8257e6',
+      white: '#fff',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
     },
   },
 });
