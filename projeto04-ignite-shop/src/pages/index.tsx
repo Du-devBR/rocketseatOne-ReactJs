dfc8dcbec0751ce73cf22c518ec9fd16bf9346b9
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-03 13:56:38 -0300

    style: Estilizado pagina home

--- projeto04-ignite-shop/src/pages/index.tsx
+++ projeto04-ignite-shop/src/pages/index.tsx
@@ -4,23 +4,21 @@ import camiseta1 from '../assets/Shirt/1.png'
 
 export default function Home() {
   return (
-    <div>
-      <HomeContainer>
-        <Product>
-          <Image src={camiseta1} alt="" width={520} height={480}/>
-          <footer>
-            <strong>Camiseta X</strong>
-            <span>R$79,70</span>
-          </footer>
-        </Product>
-        <Product>
-          <Image src={camiseta1} alt="" width={520} height={480}/>
-          <footer>
-            <strong>Camiseta X</strong>
-            <span>R$79,70</span>
-          </footer>
-        </Product>
-      </HomeContainer>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,70</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,70</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
