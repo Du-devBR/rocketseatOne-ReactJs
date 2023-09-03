795426755ba7835b34f7b0163a6576ae9e73f083
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-03 14:28:48 -0300

    feat: adicionado funcionalidade de slide para carousel de imagens

--- projeto04-ignite-shop/src/pages/index.tsx
+++ projeto04-ignite-shop/src/pages/index.tsx
@@ -1,18 +1,44 @@
+import {useKeenSlider} from 'keen-slider/react';
+
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
 import camiseta1 from '../assets/Shirt/1.png'
 
+import 'keen-slider/keen-slider.min.css'
+
+
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides :{
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480}/>
         <footer>
           <strong>Camiseta X</strong>
           <span>R$79,70</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,70</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,70</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480}/>
         <footer>
           <strong>Camiseta X</strong>
