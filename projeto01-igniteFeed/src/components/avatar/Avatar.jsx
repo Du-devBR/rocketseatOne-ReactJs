74da2ccc853f8af366bf65c3bca9e060804f2552
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-12 15:15:51 -0300

    retirado projetos

--- projeto01-igniteFeed/src/components/avatar/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-import style from  './Avatar.module.css'
-
-export function Avatar(props){
-  return(
-    <div className={style.avatar}>
-      <img src={props.src} alt="" />
-    </div>
-  )
-}
--- projeto02-igniteTimer/src/@types/styles.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components'
-import { defaultThemes } from '../styles/themes/default'
-
-type ThemeType = typeof defaultThemes;
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType{}
-}
